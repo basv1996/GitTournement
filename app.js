8247122d2095f4bc71c5ddfdcd8323133d82e6c6
juliandecloe <julian.de.cloe@hva.nl>
2022-04-14 11:57:21 +0200

    heroku fix?

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const indexRoute = require('./routes/index')
-const app = express()
-const commitsRoute = require('./routes/commits')
-const reposRoute = require('./routes/repos')
-const starsRoute = require('./routes/stars')
-const followersRoute = require('./routes/followers')
+require('dotenv').config();
+const express = require('express');
+const indexRoute = require('./routes/index');
+const app = express();
+const commitsRoute = require('./routes/commits');
+const reposRoute = require('./routes/repos');
+const starsRoute = require('./routes/stars');
+const followersRoute = require('./routes/followers');
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 module.exports = 
-  app.set('view engine', 'ejs')
-  app.set('views', './views')
+  app.set('view engine', 'ejs');
+  app.set('views', './views');
 
-  app.use(express.static('./public'))
+  app.use(express.static('./public'));
 
-  app.use('/', indexRoute)
-  app.use('/commits', commitsRoute)
-  app.use('/repos', reposRoute)
-  app.use('/stars', starsRoute)
-  app.use('/followers', followersRoute)
+  app.use('/', indexRoute);
+  app.use('/commits', commitsRoute);
+  app.use('/repos', reposRoute);
+  app.use('/stars', starsRoute);
+  app.use('/followers', followersRoute);
 
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
